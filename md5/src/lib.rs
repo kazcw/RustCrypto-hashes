1541e7d6f0d6244ada19c55cd5ee59460a6f1096
newpavlov <newpavlov@gmail.com>
2017-11-24 15:09:35 +0300

    md5 version bump

--- md5/src/lib.rs
+++ md5/src/lib.rs
@@ -7,7 +7,6 @@ extern crate byte_tools;
 extern crate block_buffer;
 #[cfg(feature = "asm")]
 extern crate md5_asm as utils;
-#[macro_use]
 extern crate digest;
 
 #[cfg(not(feature = "asm"))]
@@ -44,24 +43,19 @@ impl Default for Md5 {
 
 impl Md5 {
     #[inline]
-    fn finalize(mut self) -> [u32; 4] {
+    fn finalize(&mut self) -> [u32; 4] {
         {
             let self_state = &mut self.state;
             let l = (self.length_bytes << 3) as u64;
             self.buffer.len_padding(l, |d| compress(self_state, d));
         }
-        self.state
+        let res = self.state.clone();
+        *self = Default::default();
+        res
     }
 
     #[inline]
-    pub fn hash(self) -> [u8; 16] {
-        let mut out = [0u8; 16];
-        write_u32v_le(&mut out, &self.finalize());
-        out
-    }
-
-    #[inline]
-    pub fn consume(&mut self, input: &[u8]) {
+    fn consume(&mut self, input: &[u8]) {
         // Unlike Sha1 and Sha2, the length value in MD5 is defined as
         // the length of the message mod 2^64 - ie: integer overflow is OK.
         self.length_bytes += input.len() as u64;
@@ -85,11 +79,12 @@ impl digest::FixedOutput for Md5 {
     type OutputSize = U16;
 
     #[inline]
-    fn fixed_result(self) -> GenericArray<u8, Self::OutputSize> {
+    fn fixed_result(&mut self) -> GenericArray<u8, Self::OutputSize> {
         let mut out = GenericArray::default();
-        write_u32v_le(&mut out, &self.finalize());
+        let res = self.finalize();
+        write_u32v_le(&mut out, &res);
         out
     }
 }
 
-impl_opaque_debug!(Md5);
+//impl_opaque_debug!(Md5);
