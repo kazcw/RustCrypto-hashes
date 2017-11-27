cb32f12e305a22d8b0451ce1803a040fe236a1cf
newpavlov <newpavlov@gmail.com>
2017-11-27 16:39:05 +0300

    fixed test name

--- md2/tests/lib.rs
+++ md2/tests/lib.rs
@@ -8,7 +8,7 @@ use digest::dev::{one_million_a, digest_test};
 new_test!(md2_main, "md2", md2::Md2, digest_test);
 
 #[test]
-fn md4_1million_a() {
+fn md2_1million_a() {
     let output = include_bytes!("data/one_million_a.bin");
     one_million_a::<md2::Md2>(output);
 }
